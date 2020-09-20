30b6716e8ed0a5321d0f6a10478d22165373c8e7
David Raygosa <david_raygosa@hotmail.com>
2020-09-19 23:25:39 -0500

    Fixed App Listen

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var port = 3700;
 app.set('port', process.env.PORT || port)
 
 mongoose.Promise = global.Promise;
+/* THIS LINE TO CONNECT DB
 mongoose.connect('mongodb://localhost:27017/clinic', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 		.then
 		(
@@ -20,3 +21,10 @@ mongoose.connect('mongodb://localhost:27017/clinic', { useNewUrlParser: true, us
 		)
 
 		.catch(error => console.log(error));
+*/
+
+// THIS LINE TO CONNECT WITHOUT DB
+app.listen(app.get('port'), () => 
+{
+		console.log("Servidor Corriendo Correctamente En Puerto: "+app.get('port'));
+});
